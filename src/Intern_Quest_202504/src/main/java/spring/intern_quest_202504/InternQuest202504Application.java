8bd44a6920c4c15ac1c4151b1491eb34881b10a4
ono-Rom5-6 <atamanoiitori@gmail.com>
2025-02-03 11:57:53 +0900

    Add files via upload

---
+++ src/Intern_Quest_202504/src/main/java/spring/intern_quest_202504/InternQuest202504Application.java
@@ -0,0 +1,13 @@
+package spring.intern_quest_202504;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternQuest202504Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternQuest202504Application.class, args);
+	}
+
+}
