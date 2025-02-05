86707725d8d82f0003f7fe5793bb12c046e56b9d
ono-Rom5-6 <atamanoiitori@gmail.com>
2025-02-05 15:33:15 +0900

    ログイン機能途中

--- src/main/java/spring/intern_quest_202504/controller/LoginController.java
+++ src/main/java/spring/intern_quest_202504/controller/LoginController.java
@@ -1,5 +1,13 @@
 package spring.intern_quest_202504.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+	@GetMapping("/login")
+	public String get() {
+		return "login";
+	}
 
 }
