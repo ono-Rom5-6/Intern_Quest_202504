baad32c69d913d56f73e078d7e6bbe980ba28bd1
ono-Rom5-6 <atamanoiitori@gmail.com>
2025-02-18 12:24:53 +0900

    間違えてmainでhomeを実装中”

--- src/main/java/spring/intern_quest_202504/repository/OvertimeMapper.java
+++ src/main/java/spring/intern_quest_202504/repository/OvertimeMapper.java
@@ -1,5 +1,7 @@
 package spring.intern_quest_202504.repository;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import spring.intern_quest_202504.domain.overtime.model.Overtime;
@@ -7,5 +9,7 @@ import spring.intern_quest_202504.domain.overtime.model.Overtime;
 @Mapper
 public interface OvertimeMapper {
 	public void insertOvertime(Overtime overtime);
+	
+	public ArrayList<Overtime> selectThisOvertimeList(String userId);
 
 }
